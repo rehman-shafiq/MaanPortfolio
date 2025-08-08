6ef3edcad2ce683110c3aea5f2e83ceaba3965aa
Muhammad Rehman Shafiq <rshafiq58720772@gmail.com>
2025-08-08 17:44:24 +0500

    change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
                         <div className="absolute inset-y-0 right-4 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden" onClick={toggleNavbar}>
                                 <button type="button" command="--toggle" commandfor="mobile-menu" className="relative inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
-                                    <span className="absolute -inset-0.5  border-2 border-indigo-500" ></span>
+                                    <span className="absolute -inset-0.5" ></span>
                                     <span className="sr-only">Open main menu</span>
                                     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" className="size-6 in-aria-expanded:hidden">
                                         <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
